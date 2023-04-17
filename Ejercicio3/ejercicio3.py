67db142ac0e41ac376646eafa01a141af91a5e87
ddc12300 <116378134+ddc12300@users.noreply.github.com>
2023-04-17 21:53:44 +0200

    Comentarios añadidos/mejorados

--- Ejercicio3/ejercicio3.py
+++ Ejercicio3/ejercicio3.py
@@ -18,32 +18,39 @@ def clasificar_horas(x):
         return "Muchas"
 
 # 1. Evolución de espectadores (captura a captura) durante el periodo
-# Cargamos el csv en un dataframe escogiendo solo las columnas necesarias
+
+# Cargo el csv en un dataframe escogiendo solo las columnas necesarias para optimizar la carga
+# La función pd.read_csv() lee un archivo .csv y lo convierte en un dataframe 
 df1 = pd.read_csv("feb_23_es_simple.csv", sep="\t", usecols=["captured_at", "viewer_count"])
 
 # Convierto la columna captured_at a tipo de dato datetime
+# La función pd.to_datetime() convierte una columna a tipo de dato datetime
 df1["captured_at"] = pd.to_datetime(df1["captured_at"])
 
-# Agrupo los datos en un dataframe por captured_at y viewer count, calculando la suma de viewers
+# Agrupo los datos en un dataframe por captured_at y viewer count utilizando la función groupby(), y utilizo la función .sum() para sumar los valores de 'viewer_count' para cada captura y finalmente utilizo reset_index() para convertir los índices en una columna del nuevo dataframe que creo llamado que llamo evolucion_espectadores_df
 evolucion_espectadores_df = df1.groupby("captured_at")["viewer_count"].sum().reset_index()
 
 # guardo el resultado en un csv
+# La función .to_csv() guarda el dataframe en un archivo .csv
 evolucion_espectadores_df.to_csv("evolucion_espectadores.csv", index=False)
 
 # imprimo las primeras filas del dataset
+# La función .head() muestra las primeras filas del dataframe
 print(evolucion_espectadores_df.head())
 
 # 2. Categorías más vistas y en las que más horas de directo se han realizado
+
 # Cargamos el csv en un dataframe escogiendo solo las columnas necesarias
 df2 = pd.read_csv("feb_23_es_simple.csv", sep="\t", usecols=["game_name", "viewer_count"])
 
 # Calculo la suma total de espectadores y el número de capturas por categoría
+# Con groupby() agrupo el dataframe por categoría (game_name), luego utilizo .agg() para calcular la suma total de espectadores ('sum') y el número de capturas por streamer con ('count') para cada categoria, y finalmente utilizo reset_index() para convertir los índices en una columna del nuevo dataframe que creo llamado categorias_mas_vistas_df
 categorias_mas_vistas_df = df2.groupby("game_name")["viewer_count"].agg(['sum', 'count']).reset_index()
 
 # Renombro las columnas
 categorias_mas_vistas_df.columns = ["game_name", "total_viewers", "num_captures"]
 
-# Calcular las horas de directo aproximadas dividiendo el número de capturas por 4
+# Calculo las horas de directo aproximadas dividiendo el número de capturas por 4, ya que se hace una captura cada 15 min 60/15=4, creando una nueva columna llamada hours_direct
 categorias_mas_vistas_df["hours_direct"] = categorias_mas_vistas_df["num_captures"] / 4
 
 # Guardo el nuevo dataset en un archivo csv
@@ -53,19 +60,21 @@ categorias_mas_vistas_df.to_csv("categorias_mas_vistas.csv", index=False)
 print(categorias_mas_vistas_df.head())
 
 # 3. Evolución (captura a captura) de las categorías a lo largo del mes
-# Cargamos el csv en un dataframe escogiendo solo las columnas necesarias
+# Cargo el csv en un dataframe escogiendo solo las columnas necesarias
 df3 = pd.read_csv("feb_23_es_simple.csv", sep="\t", usecols=["captured_at", "game_name", "viewer_count"])
 
 # cambio tipo de dato captured_at de nuevo
 df3["captured_at"] = pd.to_datetime(df3["captured_at"])
 
 # Extraigo las 5 categorías con más espectadores a partir del df del ejercicio 2
+# La función .nlargest() devuelve las n filas con los valores más grandes de una columna y con tolist() lo convierto en una lista
 top5_categorias_viewers = categorias_mas_vistas_df.nlargest(5, "total_viewers")["game_name"].tolist()
 
 # Filtro el dataframe para incluir solo las 5 categorías con más espectadores
+# La función .isin() filtra el dataframe por aquellos valores que coinciden con los de la lista creada 
 df3 = df3[df3["game_name"].isin(top5_categorias_viewers)]
 
-# Calcula la evolución (captura a captura) de las categorías a lo largo del mes
+# Calculo la evolución (captura a captura) de las categorías a lo largo del mes agrupando por captured_at y game_name, y sumando los espectadores para cada categoria
 evolucion_categorias_df = df3.groupby(["captured_at", "game_name"])["viewer_count"].sum().reset_index()
 
 # Creo un nuevo csv
@@ -86,7 +95,7 @@ df4["captured_at"] = pd.to_datetime(df4["captured_at"])
 streamers_df = df4.groupby("streamer_name").agg({"viewer_count": "sum", "captured_at": "count"}).reset_index()
 streamers_df.columns = ["streamer_name", "viewer_count", "num_captures"]
 
-# Calcular las horas de directo aproximadas dividiendo el número de capturas por 4
+# Calculo las horas de directo aproximadas dividiendo el número de capturas por 4
 streamers_df["hours_streamed"] = streamers_df["num_captures"] / 4
 
 # Creo un nuevo csv a partir del dataframe
@@ -102,6 +111,7 @@ df5 = pd.read_csv("feb_23_es_simple.csv", sep="\t", usecols=["captured_at", "vie
 df5["captured_at"] = pd.to_datetime(df5["captured_at"])
 
 # Agrupo los datos por la columna "captured_at" y calculo la desviación estándar de espectadores
+# La función .std() calcula la desviación estándar de una columna
 desviacion_espectadores_df = df5.groupby("captured_at")["viewer_count"].std().reset_index()
 desviacion_espectadores_df.to_csv("desviacion_espectadores.csv", index=False)
 
