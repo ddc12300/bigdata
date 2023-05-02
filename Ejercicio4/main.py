c3b8165c999212b60f4a5e5e7f26cbaaf0c4a44e
Daniel <1565778@uab.cat>
2023-05-02 16:38:20 +0200

    añado codigo para crear csv hastags

--- Ejercicio4/main.py
+++ Ejercicio4/main.py
@@ -6,6 +6,9 @@ from glob import glob
 # Creo una lista vacía para almacenar los datos de los tweets procesados
 mis_datos = []
 
+# Creo una lista vacía para almacenar los datos de los hashtags procesados
+hashtags_datos = []
+
 # Itero sobre todos los archivos JSON en la carpeta 'api_responses'
 for archivo in glob("api_responses/*.json"):
     # Abro y leo el archivo JSON
@@ -34,8 +37,13 @@ for archivo in glob("api_responses/*.json"):
             if "entities" in tweet and "hashtags" in tweet["entities"]:
                 hashtags = [hashtag["tag"] for hashtag in tweet["entities"]["hashtags"]]
 
+            # Añadir hashtags y tweet_id a hashtags_datos
+            for hashtag in hashtags:
+                hashtags_datos.append({"tweet_id": tweet["id"], "hashtag": hashtag})
+
             # Creo un diccionario con la información procesada del tweet
             tweet_procesado = {
+                "tweet_id": tweet["id"],
                 "text": tweet["text"],
                 "author_id": author_id,
                 "username": username,
@@ -50,22 +58,6 @@ for archivo in glob("api_responses/*.json"):
                 "lang": tweet["lang"],
             }
 
-            # Extraigo menciones
-
-            # Si el tweet contiene un campo llamado "entities" y dentro de "entities" hay otro campo llamado "mentions", entonces procedo a extraer las menciones.
-            if "entities" in tweet and "mentions" in tweet["entities"]:
-
-                # Las menciones son otros usuarios de Twitter que han sido mencionados en el tweet.
-                # tweet_procesado["mentions"] es una lista que contendrá los nombres de usuario de las menciones.
-                # Utilizo una comprensión de lista para iterar sobre cada mención en tweet["entities"]["mentions"].
-                # Para cada mención, extraigo el nombre de usuario utilizando mention["username"] y lo agrego a la lista.
-
-                tweet_procesado["mentions"] = [mention["username"] for mention in tweet["entities"]["mentions"]]
-            # Si no hay "entities" o "mentions" en el tweet, entonces no hay menciones.
-            else:
-                # En este caso, asigno una lista vacía a tweet_procesado["mentions"] para indicar que no hay menciones en este tweet.
-                tweet_procesado["mentions"] = []
-
             # Añado el tweet procesado a mi lista de datos
             mis_datos.append(tweet_procesado)
 
@@ -78,25 +70,8 @@ df['text'] = df['text'].str.replace('\n', ' ').replace('\r', ' ')
 # Guardar el DataFrame en un archivo CSV utilizando el carácter de tabulación '\t' como delimitador
 df.to_csv("tweets.csv", index=False, sep='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-# Extraigo menciones y las guardo en un archivo CSV para Gephi
+# Crear DataFrame de hashtags
+hashtags_df = pd.DataFrame(hashtags_datos)
 
-# Creo una lista vacía llamada edges
-# Esta lista almacenará las relaciones entre los usuarios y las menciones en los tweets.
-# Cada elemento de la lista será una tupla que contiene el nombre de usuario del autor del tweet (Source) y el nombre de usuario de la mención (Target).
-edges = []
-
-# Itero sobre cada fila del dataframe creado anteriormente
-for _, fila in df.iterrows():
-    # Extraigo el nombre de usuario del autor del tweet de la fila actual y lo guardo en la variable 'user'
-    user = fila["username"]
-
-    # Añado cada mención como una tupla de (usuario, mención) a la lista de 'edges'
-    # Itero sobre cada mención en la columna "mentions" de la fila actual
-    for mention in fila["mentions"]:
-        # Creo una tupla con el nombre de usuario del autor del tweet (user) y el nombre de usuario de la mención (mention)
-        # Añado la tupla a la lista 'edges'
-        edges.append((user, mention))
-
-# Creo un DataFrame con las relaciones de menciones y lo guardo en un archivo CSV
-edges_df = pd.DataFrame(edges, columns=["Source", "Target"])
-edges_df.to_csv("edges_gephi.csv", index=False)
+# Guardar el DataFrame de hashtags en un archivo CSV
+hashtags_df.to_csv("hashtags.csv", index=False)
